468012b5cd82993c97df02569e9424d9905a50a0
Manan Sharma <manansharma177@gmail.com>
2022-07-22 03:07:06 +0530

    updated label

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import youtube from "../api/youtube";
 
@@ -23,29 +22,3 @@ const useVideos = (defaultSearchTerm) => {
 };
 
 export default useVideos;
-=======
-import { useState, useEffect } from "react";
-import youtube from "../api/youtube";
-
-const useVideos = (defaultSearchTerm) => {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    search(defaultSearchTerm);
-  }, [defaultSearchTerm]);
-
-  const search = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    setVideos(response.data.items);
-  };
-
-  return [videos, search];
-};
-
-export default useVideos;
->>>>>>> 41ab3b9d7939ed5f5daf9249d883e7d8e7cd2021
